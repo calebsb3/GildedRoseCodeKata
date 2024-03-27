23650f68a31289fc68c6b953645cb47425256fd7
Caleb Belk <caleb8814@gmail.com>
2024-03-26 21:17:51 -0600

    add constructor

--- Item.cs
+++ Item.cs
@@ -1,5 +1,11 @@
 public class Item {
+    public Item(string newName, int newQuality, int DaysToSell){
+        Name = newName;
+        Quality = newQuality;
+        NumDaysToSell = DaysToSell;
+    }
     public string Name {get;}
     public int Quality {get; set;}
     public int NumDaysToSell {get; set;}
+
 }

dfa9b78aad45b824431af1105bd91c25d90b0c19
Caleb Belk <caleb8814@gmail.com>
2024-03-26 21:17:39 -0600

    add simulated program

--- Program.cs
+++ Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var storefront = new Storefront(){
+            ItemsForSale = new List<Item>() {
+                new Item("Sulfuras", 80, 8000),
+                new Item("Aged Brie", 0, 25),
+                new Item("Backstage passes", 0, 50),
+                new Item("Crackers", 15, 90),
+                new Item("Conjured Crackers", 15, 90)
+            }
+        };
+
+        for (int i = 0; i < 30; i++)
+        {
+            storefront.Update();
+            storefront.printItems();
+        }
     }
 }
